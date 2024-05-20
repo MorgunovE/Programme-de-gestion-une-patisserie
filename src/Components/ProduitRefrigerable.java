bfec3e52abb610bdec8c7e9163c798b142b1b308
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-05-20 17:58:44 -0400

    add public class ProduitRefrigerable extends Produit et implement constructor getter setter

---
+++ src/Components/ProduitRefrigerable.java
@@ -0,0 +1,18 @@
+package Components;
+
+public class ProduitRefrigerable extends Produit{
+    private double temperatureDeRefrigeration;
+
+    public ProduitRefrigerable(String nom, String code, double prix, double poids, double temperatureDeRefrigeration) {
+        super(nom, code, prix, poids);
+        this.temperatureDeRefrigeration = temperatureDeRefrigeration;
+    }
+
+    public double getTemperatureDeRefrigeration() {
+        return temperatureDeRefrigeration;
+    }
+
+    public void setTemperatureDeRefrigeration(double temperatureDeRefrigeration) {
+        this.temperatureDeRefrigeration = temperatureDeRefrigeration;
+    }
+}
