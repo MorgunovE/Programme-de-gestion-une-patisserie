b0c8b0c747d9f14cf2747bb5114d9f54c5a51da1
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-05-20 17:46:06 -0400

    add public class ProduitEmballable extends Produit implement comstructor et setter

---
+++ src/Components/ProduitEmballable.java
@@ -0,0 +1,18 @@
+package Components;
+
+public class ProduitEmballable extends Produit{
+    private boolean emballageBiodegradable;
+
+    public ProduitEmballable(String nom, String code, double prix, double poids, boolean emballageBiodegradable) {
+        super(nom, code, prix, poids);
+        this.emballageBiodegradable = emballageBiodegradable;
+    }
+
+    public boolean isEmballageBiodegradable() {
+        return emballageBiodegradable;
+    }
+
+    public void setEmballageBiodegradable(boolean emballageBiodegradable) {
+        this.emballageBiodegradable = emballageBiodegradable;
+    }
+}
