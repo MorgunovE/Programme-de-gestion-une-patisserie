d994bc9f360dd9fe063773db9615ca19b3850eb3
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-05-20 19:52:25 -0400

    add public class EntrepriseEtiquetage et method de ajoute etiquette

---
+++ src/Components/Entreprise/EntrepriseEtiquetage.java
@@ -0,0 +1,23 @@
+package Components.Entreprise;
+
+import Components.Produit.Produit;
+
+public class EntrepriseEtiquetage {
+    private String nom;
+    private String siteWeb;
+    private String coordonnees;
+
+    public EntrepriseEtiquetage(String nom, String siteWeb, String coordonnees) {
+        this.nom = nom;
+        this.siteWeb = siteWeb;
+        this.coordonnees = coordonnees;
+    }
+
+    public void etiqueterProduit(Produit produit) {
+        String etiquette = "Nom du produit : " + produit.getNom() + '\n' +
+                "Nom de l'entreprise : " + this.nom + '\n' +
+                "Site web de l'entreprise : " + this.siteWeb + '\n' +
+                "Coordonnees de l'entreprise : " + this.coordonnees + '\n';
+        produit.setEtiquette(etiquette);
+    }
+}
