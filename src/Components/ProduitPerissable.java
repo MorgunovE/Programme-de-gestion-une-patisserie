b09691e54630d3386c90d988ed3ea01916544ec2
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-05-20 17:51:39 -0400

    add public class ProduitPerissable extends Produit et implement constructor getter setter

---
+++ src/Components/ProduitPerissable.java
@@ -0,0 +1,18 @@
+package Components;
+
+public class ProduitPerissable extends Produit{
+    private int dureeDeVie;
+
+    public ProduitPerissable(String nom, String code, double prix, double poids, int dureeDeVie) {
+        super(nom, code, prix, poids);
+        this.dureeDeVie = dureeDeVie;
+    }
+
+    public int getDureeDeVie() {
+        return dureeDeVie;
+    }
+
+    public void setDureeDeVie(int dureeDeVie) {
+        this.dureeDeVie = dureeDeVie;
+    }
+}
