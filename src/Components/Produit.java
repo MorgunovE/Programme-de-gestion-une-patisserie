9407e32b784af550e9deba7d647fdc4a3fb5a7df
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-05-20 17:35:49 -0400

    add public class Produit.java and implement constructor getters and setters

---
+++ src/Components/Produit.java
@@ -0,0 +1,47 @@
+package Components;
+
+public class Produit {
+    private String nom;
+    private String code;
+    private double poids;
+    private double prix;
+
+    public Produit(String nom, String code, double prix, double poids) {
+        this.prix = prix;
+        this.nom = nom;
+        this.code = code;
+        this.poids = poids;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public double getPoids() {
+        return poids;
+    }
+
+    public void setPoids(double poids) {
+        this.poids = poids;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+}
