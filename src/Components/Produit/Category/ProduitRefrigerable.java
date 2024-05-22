0eed4a35ee6e600a26fe2426177205c742c3a6fb
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-05-21 20:26:15 -0400

    add commentaires dans public class ProduitRefrigerable

--- src/Components/Produit/Category/ProduitRefrigerable.java
+++ src/Components/Produit/Category/ProduitRefrigerable.java
@@ -2,18 +2,41 @@ package Components.Produit.Category;
 
 import Components.Produit.Produit;
 
+/**
+ * Classe représentant un produit réfrigérable.
+ * Un produit réfrigérable est un produit qui nécessite une température de réfrigération spécifique.
+ */
 public class ProduitRefrigerable extends Produit {
     private double temperatureDeRefrigeration;
 
+    /**
+     * Constructeur de la classe ProduitRefrigerable.
+     *
+     * @param nom                      le nom du produit
+     * @param code                     le code du produit
+     * @param prix                     le prix du produit
+     * @param poids                    le poids du produit
+     * @param temperatureDeRefrigeration la température de réfrigération du produit
+     */
     public ProduitRefrigerable(String nom, String code, double prix, double poids, double temperatureDeRefrigeration) {
         super(nom, code, prix, poids);
         this.temperatureDeRefrigeration = temperatureDeRefrigeration;
     }
 
+    /**
+     * Retourne la température de réfrigération du produit.
+     *
+     * @return la température de réfrigération du produit
+     */
     public double getTemperatureDeRefrigeration() {
         return temperatureDeRefrigeration;
     }
 
+    /**
+     * Définit la température de réfrigération du produit.
+     *
+     * @param temperatureDeRefrigeration la température de réfrigération du produit
+     */
     public void setTemperatureDeRefrigeration(double temperatureDeRefrigeration) {
         this.temperatureDeRefrigeration = temperatureDeRefrigeration;
     }
