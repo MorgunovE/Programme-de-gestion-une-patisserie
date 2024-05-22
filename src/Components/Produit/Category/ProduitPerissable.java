c3fdb1f4058254e2501556a4f90e7e2ba82d3f96
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-05-21 20:55:49 -0400

    add methods dans public class ProduitPerissable

--- src/Components/Produit/Category/ProduitPerissable.java
+++ src/Components/Produit/Category/ProduitPerissable.java
@@ -2,12 +2,15 @@ package Components.Produit.Category;
 
 import Components.Produit.Produit;
 
+import java.time.LocalDate;
+
 /**
  * Classe représentant un produit périssable.
  * Un produit périssable est un produit qui a une durée de vie limitée.
  */
 public class ProduitPerissable extends Produit {
     private int dureeDeVie;
+    private LocalDate dateDePeremption;
 
     /**
      * Constructeur de la classe ProduitPerissable.
@@ -21,6 +24,7 @@ public class ProduitPerissable extends Produit {
     public ProduitPerissable(String nom, String code, double prix, double poids, int dureeDeVie) {
         super(nom, code, prix, poids);
         this.dureeDeVie = dureeDeVie;
+        this.dateDePeremption = LocalDate.now().plusDays(dureeDeVie);
     }
 
     /**
@@ -39,5 +43,29 @@ public class ProduitPerissable extends Produit {
      */
     public void setDureeDeVie(int dureeDeVie) {
         this.dureeDeVie = dureeDeVie;
+    }
+
+    /**
+     * Élimine le produit si sa date de péremption est dépassée.
+     *
+     * @return une chaîne de caractères indiquant si le produit a été éliminé ou non
+     */
+    public String eliminerProduit() {
+        if (LocalDate.now().isAfter(dateDePeremption)) {
+            return "Le produit " + getNom() + " a été éliminé car sa date de péremption est dépassée..";
+        }
+        return "Le produit " + getNom() + " n'a pas été éliminé car sa date de péremption n'est pas encore dépassée.";
+    }
+
+    /**
+     * Vérifie l'état actuel du produit et identifie s'il est toujours consommable.
+     *
+     * @return une chaîne de caractères indiquant si le produit est consommable ou non
+     */
+    public String verifierEtat() {
+        if (LocalDate.now().isAfter(dateDePeremption)) {
+            return "Le produit " + getNom() + " n'est plus consommable car sa date de péremption est dépassée..";
+        }
+        return "Le produit " + getNom() + " est toujours consommable.";
     }
 }
