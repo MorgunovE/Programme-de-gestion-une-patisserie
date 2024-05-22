0c98fcd90b1b017ff84c9a4cc3ba30d800341abc
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-05-21 20:24:06 -0400

    add commentaires dans public class EntrepriseEtiquetage

--- src/Components/Entreprise/EntrepriseEtiquetage.java
+++ src/Components/Entreprise/EntrepriseEtiquetage.java
@@ -2,17 +2,33 @@ package Components.Entreprise;
 
 import Components.Produit.Produit;
 
+/**
+ * Classe représentant une entreprise d'étiquetage.
+ * Une entreprise d'étiquetage est responsable de l'étiquetage des produits.
+ */
 public class EntrepriseEtiquetage {
     private String nom;
     private String siteWeb;
     private String coordonnees;
 
+    /**
+     * Constructeur de la classe EntrepriseEtiquetage.
+     *
+     * @param nom         le nom de l'entreprise
+     * @param siteWeb     le site web de l'entreprise
+     * @param coordonnees les coordonnées de l'entreprise
+     */
     public EntrepriseEtiquetage(String nom, String siteWeb, String coordonnees) {
         this.nom = nom;
         this.siteWeb = siteWeb;
         this.coordonnees = coordonnees;
     }
 
+    /**
+     * Étiquette un produit avec les informations de l'entreprise.
+     *
+     * @param produit le produit à étiqueter
+     */
     public void etiqueterProduit(Produit produit) {
         String etiquette = "Nom du produit : " + produit.getNom() + '\n' +
                 "Code du produit : " + produit.getCode() + '\n' +
