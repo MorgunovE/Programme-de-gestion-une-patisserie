4fb2f5c993f5ef55799b6c3da956099313e218cf
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-05-21 20:47:04 -0400

    add methods dans public class ProduitEmballable

--- src/Components/Produit/Category/ProduitEmballable.java
+++ src/Components/Produit/Category/ProduitEmballable.java
@@ -4,10 +4,12 @@ import Components.Produit.Produit;
 
 /**
  * Classe représentant un produit emballable.
- * Un produit emballable est un produit qui a un emballage biodégradable.
+ * Un produit emballable est un produit qui a un emballage biodégradable ou recyclable et doit être emballé avant d'être mis en vente.
  */
 public class ProduitEmballable extends Produit {
     private boolean emballageBiodegradable;
+    private boolean estEmballer;
+    private boolean estRecyclable;
 
     /**
      * Constructeur de la classe ProduitEmballable.
@@ -17,10 +19,20 @@ public class ProduitEmballable extends Produit {
      * @param prix                 le prix du produit
      * @param poids                le poids du produit
      * @param emballageBiodegradable si l'emballage est biodégradable ou non
+     * @param estRecyclable si l'emballage est recyclable ou non
      */
-    public ProduitEmballable(String nom, String code, double prix, double poids, boolean emballageBiodegradable) {
+    public ProduitEmballable(String nom, String code, double prix, double poids, boolean emballageBiodegradable, boolean estRecyclable) {
         super(nom, code, prix, poids);
         this.emballageBiodegradable = emballageBiodegradable;
+        this.estEmballer = false;
+        this.estRecyclable = estRecyclable;
+    }
+
+    /**
+     * Emballe le produit.
+     */
+    public void emballer() {
+        this.estEmballer = true;
     }
 
     /**
@@ -39,5 +51,40 @@ public class ProduitEmballable extends Produit {
      */
     public void setEmballageBiodegradable(boolean emballageBiodegradable) {
         this.emballageBiodegradable = emballageBiodegradable;
+    }
+
+    /**
+     * Retourne si l'emballage est recyclable ou non.
+     *
+     * @return vrai si l'emballage est recyclable, faux sinon
+     */
+    public boolean isEstRecyclable() {
+        return estRecyclable;
+    }
+
+    /**
+     * Définit si l'emballage est recyclable ou non.
+     *
+     * @param estRecyclable si l'emballage est recyclable ou non
+     */
+    public void setEstRecyclable(boolean estRecyclable) {
+        this.estRecyclable = estRecyclable;
+    }
+
+    /**
+     * Valide l'état de l'emballage.
+     *
+     * @return une chaîne de caractères représentant l'état de l'emballage
+     */
+    public String validerEtatEmballage() {
+        if (!this.estEmballer) {
+            return "Le produit n'est pas encore emballé.";
+        } else if(this.emballageBiodegradable){
+            return "Le produit est emballé avec un emballage biodégradable.";
+        } else if(this.estRecyclable){
+            return "Le produit est emballé avec un emballage recyclable.";
+        } else {
+            return "Le produit est emballé, mais l'emballage n'est ni biodégradable ni recyclable.";
+        }
     }
 }
